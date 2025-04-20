c74c947c7cd639aa0ff5471f7d5f18dff5a69c1e
EmanuelIna <emanuelinacio052@gmail.com>
2025-04-20 18:21:04 -0400

    Sort and Filters

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react';
 
 import Todo from './components/Todo';
+import Search from './components/Search';
+import TodoForm from './components/TodoForm';
 
 import './App.css'
-import TodoForm from './components/TodoForm';
-import Search from './components/Search';
+import Filter from './components/filter';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -29,6 +30,9 @@ function App() {
   ]);
 
   const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState("All");
+  const [sort, setSort] = useState("Asc");
+
 
   const addTodo = (text, category) => {
     const newTodos = [...todos, 
@@ -62,10 +66,24 @@ function App() {
     <div className='app'>
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch = {setSearch}/>
+      <Filter filter = {filter} setFilter = {setFilter} setSort ={setSort} />
       <div className='todo-list'>
         {todos
         .filter((todo) => 
-          todo.text.toLowerCase().includes(search.toLowerCase()))
+          filter === "All" 
+          ? true
+          : filter === "Completed" 
+          ? todo.isCompleted
+          : !todo.isCompleted
+        )
+        .filter((todo) => 
+          todo.text.toLowerCase().includes(search.toLowerCase())
+      )
+        .sort((a, b) => 
+          sort === "Asc" 
+        ? a.text.localeCompare(b.text) 
+        : b.text.localeCompare(a.text)
+      )
         .map((todo) => (
             <Todo key = {todo.id} todo = {todo} removeTodo = {removeTodo} completeTodo ={completeTodo}/>          
           ))}
